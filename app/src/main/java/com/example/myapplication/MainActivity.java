62c04f094248f9e63f98d906e0e10e26c2b7e163
whiteLi12 <1558145521@qq.com>
2019-03-20 19:50:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("MainActivity","调用onCreate()");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("MainActivity","调用onStart()");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("MainActivity","调用onResume()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("MainActivity","调用onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("MainActivity","调用onStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("MainActivity","调用onDestroy()");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("MainActivity","调用ononRestart()");
+    }
+}
